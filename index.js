85bb5c46f304ac152a50328ab0c8b6ad6cff71e7
Leo Young <420760135@qq.com>
2018-09-24 20:31:47 +0800

    init

---
+++ index.js
@@ -0,0 +1,17 @@
+var net = require('net');
+var server = net.createServer((socket) => {
+  console.log('client connected');
+  socket.on('data', (data) => {
+    console.log('receive data: ' + data.toString());
+  });
+  socket.on('end', () => {
+    console.log('client disconnected');
+  });
+  socket.write('hello, world\n');
+});
+server.on('error', (err) => {
+  console.log(err);
+});
+server.listen(7269, () => {
+  console.log('server is listening on port: 7269');
+});
