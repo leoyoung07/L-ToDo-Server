3c15cc8b982fc0bd3c0e12eec55f7fda3f748c9f
Leo Young <420760135@qq.com>
2018-10-03 19:50:04 +0800

    feat: download/upload tasks

--- index.js
+++ index.js
@@ -1,21 +1,64 @@
 var net = require('net');
 var Redis = require('ioredis');
 var redis = new Redis();
-var server = net.createServer((socket) => {
+var server = net.createServer(socket => {
   console.log('client connected');
-  socket.on('data', (data) => {
-    data = data.toString();
-    console.log('receive data: ' + data);
-    if (data.indexOf('hello') !== 0) {
-      redis.set('tasks', data);
+  socket.on('data', buffer => {
+    const reqStr = buffer.toString();
+    console.log('receive data: ' + reqStr);
+    const request = JSON.parse(reqStr);
+    switch (request.type) {
+      case 'download':
+        redis.get('tasks', (error, data) => {
+          if (error) {
+            socket.write(
+              JSON.stringify({
+                type: 'error',
+                data: error
+              })
+            );
+          } else {
+            socket.write(
+              JSON.stringify({
+                type: 'download',
+                data: data
+              })
+            );
+          }
+        });
+        break;
+      case 'upload':
+        redis.set('tasks', request.data);
+        socket.write(
+          JSON.stringify({
+            type: 'upload',
+            data: 'success'
+          })
+        );
+        break;
+      case 'ping':
+        socket.write(
+          JSON.stringify({
+            type: 'pong',
+            data: 'pong'
+          })
+        );
+        break;
+      default:
+        socket.write(
+          JSON.stringify({
+            type: 'error',
+            data: 'Unknown request type.'
+          })
+        );
+        break;
     }
   });
   socket.on('end', () => {
     console.log('client disconnected');
   });
-  socket.write('hello, client\n');
 });
-server.on('error', (err) => {
+server.on('error', err => {
   console.log(err);
 });
 server.listen(7269, () => {
